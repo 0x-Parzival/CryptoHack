9ee5ed4911e8cdd30882261434e6400259084bd6
0xParzival <145645180+0x-Parzival@users.noreply.github.com>
2024-11-07 12:24:13 +0530

    Create Modular Exponentiation.py

---
+++ Public-Key Cryptography/Modular Exponentiation.py
@@ -0,0 +1,5 @@
+# Let's compute the value of 10^117 mod 22663 using Python's built-in pow function.
+# This will help us find the solution to the modular exponentiation problem.
+
+result = pow(10, 117, 22663)
+result
